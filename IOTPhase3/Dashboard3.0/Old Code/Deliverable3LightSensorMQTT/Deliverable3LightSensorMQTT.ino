772d3efacdad1d6cdd664c51016d9a3206e9b9da
Joseph C <112569023+Joseph-AC@users.noreply.github.com>
2024-11-18 15:00:57 -0500

    fixes to the 3rd deliverable (NOT DONE FINISH HTML VIEW)

---
+++ IOTPhase3/Dashboard3.0/Old Code/Deliverable3LightSensorMQTT/Deliverable3LightSensorMQTT.ino
@@ -0,0 +1,107 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "";
+const char* password = "";
+// Add your MQTT Broker IP address, example:
+const char* mqtt_server = "";
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+// LED Pin
+
+#define LIGHT_SENSOR_PIN  35 // ESP32 pin GPIO36 (ADC0) connected to light sensor
+#define LED_PIN           26  // ESP32 pin GPIO22 connected to LED
+#define ANALOG_THRESHOLD  2000
+void setup() {
+ Serial.begin(115200);
+ setup_wifi();
+//Install this library
+ client.setServer(mqtt_server, 1883);
+analogSetAttenuation(ADC_11db);
+  pinMode(LED_PIN, OUTPUT);
+}
+void setup_wifi() {
+ delay(10);
+ // We start by connecting to a WiFi network
+ Serial.println();
+ Serial.print("Connecting to ");
+ Serial.println(ssid);
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+ delay(500);
+ Serial.print(".");
+ }
+ Serial.println("");
+ Serial.println("WiFi connected");
+ Serial.println("IP address: ");
+ Serial.println(WiFi.localIP());
+}
+/*
+void callback(char* topic, byte* message, unsigned int length) {    
+ Serial.print("Message arrived on topic: ");
+ Serial.print(topic);
+ Serial.print(". Message: ");
+ String messagein;
+
+ for (int i = 0; i < length; i++) {
+ Serial.print((char)message[i]);
+ messagein += (char)message[i];
+ }
+ if(topic=="room/light"){
+ if (messagein == "ON")
+ Serial.println("Light is ON");
+ }else{
+ Serial.println("Light is OFF");
+ }
+ 
+}*/
+void reconnect() {
+ while (!client.connected()) {
+ Serial.print("Attempting MQTT connection...");
+ if (client.connect("vanieriot")) {
+ Serial.println("connected vanieriot");
+ client.subscribe("room/light",0);
+ 
+ } else {
+ Serial.print("failed, rc=");
+ Serial.print(client.state());
+ Serial.println(" try again in 5 seconds");
+ // Wait 5 seconds before retrying
+ delay(5000);
+ }
+ }
+}
+void loop() {
+ if (!client.connected()) {
+ reconnect();
+ }
+
+ if(!client.loop())
+ client.connect("vanieriot");
+
+ //client.subscribe("room/light",0); 
+
+int analogValue = analogRead(LIGHT_SENSOR_PIN); // read the value on analog pin
+    Serial.println(analogValue);
+    //to convert
+    char message[10];
+    itoa(analogValue, message, 10);
+    client.publish("IoTlab/INTENSITY",message);
+
+  if (analogValue < ANALOG_THRESHOLD){
+     Serial.println("Message published: It is DARK");
+    client.publish("IoTlab/SENSOR","Light:ON");
+   // digitalWrite(LED_PIN, HIGH); // turn on LED
+  //  Serial.print("HIGH");
+  }
+  else{
+     Serial.println("Message published: Not dark");
+    client.publish("IoTlab/SENSOR","Light:OFF");
+   // digitalWrite(LED_PIN, LOW);  // turn off LED
+    //Serial.print("LOW");
+  }
+ delay(1000);
+ }
--- IOTPhase3/Dashboard3.0/app.py
+++ IOTPhase3/Dashboard3.0/app.py
@@ -5,7 +5,10 @@ import RPi.GPIO as GPIO
 import time
 from time import sleep
 import paho.mqtt.client as mqtt
+from flask import Flask, jsonify, render_template
 
+
+app = Flask(__name__)
 
 LED = 17  
 GPIO.setmode(GPIO.BCM)
@@ -20,7 +23,7 @@ print("LED is OFF - Initial state.")
 
 #Info for MQTTSettings
 # MQTT Settings
-mqtt_broker = "PUT_IP_HERE"  # IP of the MQTT broker (insert)
+mqtt_broker = ""  # IP of the MQTT broker (insert)
 mqtt_port = 1883  
 mqtt_topic = "IoTlab/SENSOR"  # The topic to subscribe  for the sensor itself (ON/OFF)
 mqtt_topic2 = "IoTlab/INTENSITY"  # The topic to subscribe for the intensity (1023)
@@ -57,20 +60,21 @@ client.subscribe(mqtt_topic)
 client.subscribe(mqtt_topic2)
 
 # Start a background thread to listen for messages
-client.loop_start()
+#client.loop_start()
 #-------------------------MQTT_SERVER----------------------------------
 
 #-------------------------EMAIL----------------------------------
 
 # Email setup
-GMAIL_USER = '@gmail.com'  
-GMAIL_PASSWORD = 'ukne' 
-RECEIVER_EMAIL = '@gmail.com'  
+GMAIL_USER = 'seconddummytwo@gmail.com'  
+GMAIL_PASSWORD = 'yucp cxwh gycz pena' 
+RECEIVER_EMAIL = 'templatebuttondown@gmail.com'  
 
 
 def send_email():
     """Send an email notification."""
     try:
+        
         current_time = datetime.now().strftime("%H:%M")
         subject = "Light Notification"
         body = f"The Light is ON at {current_time}."
@@ -80,7 +84,7 @@ def send_email():
         message["Subject"] = subject
         message["From"] = GMAIL_USER
         message["To"] = RECEIVER_EMAIL
-        
+        print("EMAIL SENDING")
         # Send email
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
@@ -116,6 +120,16 @@ def send_email():
 
 # 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sensor-data')
+def get_sensor_data():
+    return jsonify({
+                    'intensity': intensityData,
+                })
+
 try:
     while True:
         # Check the received MQTT message and control the LED based on the message
@@ -123,15 +137,25 @@ try:
             GPIO.output(LED, GPIO.HIGH)  # Turn on the LED if the message indicates it is dark
             send_email()
         elif mqtt_message == "Light:OFF":
+            print("TURN OFF-----------------")
             GPIO.output(LED, GPIO.LOW)  # Turn off the LED if the message indicates it is not dark
         sleep(1)  # Sleep for 1 second
 
 except KeyboardInterrupt:
     print("Program interrupted")
+    
+
 
 finally:
     # Cleanup GPIO and stop MQTT client when exiting
     GPIO.cleanup()
     client.loop_stop()  # Stop the MQTT client loop
     client.disconnect()  # Disconnect from the MQTT broker
-
+    
+if __name__ == '__main__':
+    while True:
+        try:
+            app.run(host='0.0.0.0', port=5001)
+            time.sleep(10)  # Wait for 10 seconds before the next check
+        finally:
+            sensor.exit()
