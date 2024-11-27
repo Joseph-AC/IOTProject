134d35f93be66be1c46024ab02421f34cb48ab15
WillitC <117682514+WillitC@users.noreply.github.com>
2024-11-27 00:44:54 -0500

    Update main.py

--- IOTPhase4/Dashboard4.0/main.py
+++ IOTPhase4/Dashboard4.0/main.py
@@ -2,26 +2,27 @@
 from flask import Flask, jsonify, render_template
 
 import RFID
-
-
+import LED
+import MQTT_Manager
 
 app = Flask(__name__)
-
-intensityData = 300
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/sensor-data')
+@app.route('/LED')
 def get_sensor_data():
-    return jsonify({
-                    'intensity': 1500,#intensityData,
-                })
+    return jsonify(LED.LEDData())
 
 @app.route('/profile')
 def get_profile_data():
     return jsonify(RFID.profileData())
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    try:
+        MQTT_Manager.start_MQTT()
+        app.run(host='0.0.0.0', port=5001)
+
+    finally:
+        MQTT_Manager.stop_MQTT()
